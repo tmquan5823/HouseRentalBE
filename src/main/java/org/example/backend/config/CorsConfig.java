7421e8d26154ce73d43d9dcab31c7fbd92e2a795
Trần Minh Quân <104264023+tmquan5823@users.noreply.github.com>
2025-02-14 14:30:32 +0700

    Update CorsConfig.java

--- src/main/java/org/example/backend/config/CorsConfig.java
+++ src/main/java/org/example/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://house-rental-fe.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
